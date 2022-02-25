46b098d63328cc43a805ef367c59172435cd0634
Nik Lebedenko <nl419@cam.ac.uk>
2022-02-25 10:08:47 +0000

    moved calibrate to cv

---
+++ cv/capture.py
@@ -0,0 +1,20 @@
+"""Take screenshots of the camera video with w, quit with q"""
+
+import cv2
+
+folder = "checkerboard2"
+
+print ("starting grab")
+cap = cv2.VideoCapture('http://localhost:8081/stream/video.mjpeg')
+counter = 1
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('Video', frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+    elif key == ord('w'):
+        cv2.imwrite(folder + "/" + str(counter) + ".jpg", frame)
+        counter += 1
+cap.release()
+cv2.destroyAllWindows()
