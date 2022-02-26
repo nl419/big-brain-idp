4cf245f961a6ca9281bce6a5c1fb32b64e2e5094
Nik Lebedenko <nl419@cam.ac.uk>
2022-02-26 21:46:21 +0000

    masking

--- cv/qr_tracking.py
+++ cv/qr_tracking.py
@@ -11,16 +11,22 @@ from find_qr import *
 # If tracking lost for 5 frames in a row, go to step 1
 
 video_file = 'test_vids/qr2.mp4'
+TRACKER = False  # Whether to use an ROI tracking method
 
-if __name__ == '__main__' :
-    tracker_types = ['KCF', 'CSRT']
-    tracker_index = 1
+if __name__ == '__main__':
+    if TRACKER:
+        tracker_types = ['KCF', 'CSRT']
+        tracker_index = 1
 
-    tracker_type = tracker_types[tracker_index]
-    tracker_funcs = [cv2.TrackerKCF_create, cv2.TrackerCSRT_create]
-    tracker = tracker_funcs[tracker_index]()
-    
-    print("Selected tracker type", tracker_type)
+        tracker_type = tracker_types[tracker_index]
+        tracker_funcs = [cv2.TrackerKCF_create, cv2.TrackerCSRT_create]
+        tracker = tracker_funcs[tracker_index]()
+
+        print("Selected tracker type", tracker_type)
+    else:
+        lastCentre = None
+        track_timeout = 5  # How many frames of consecutive tracking failure before resetting lastCentre
+        track_fail_count = 0
 
     # Read video
     video = cv2.VideoCapture(video_file)
@@ -33,62 +39,82 @@ if __name__ == '__main__' :
     # Read first frame.
     ok, frame = video.read()
     if not ok:
-        print ('Cannot read video file')
+        print('Cannot read video file')
         sys.exit()
 
     qrDecoder = cv2.QRCodeDetector()
 
-    # Uncomment the line below to select a different bounding box
-    roi = cv2.selectROI(frame, False)
-
-    # Initialize tracker with first frame and bounding box
-    ok = tracker.init(frame, roi)
+    if TRACKER:
+        roi = cv2.selectROI(frame, False)
+        # Initialize tracker with first frame and bounding box
+        ok = tracker.init(frame, roi)
 
     while True:
         # Read a new frame
         ok, frame = video.read()
         if not ok:
             break
-        
+
         # Start timer
         timer = cv2.getTickCount()
 
-        ok, roi = tracker.update(frame)
+        if TRACKER:
+            ok, roi = tracker.update(frame)
 
-        # Draw bounding box
-        if ok:
-            # Tracking success
-            p1 = (int(roi[0]), int(roi[1]))
-            p2 = (int(roi[0] + roi[2]), int(roi[1] + roi[3]))
-            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+            # Draw bounding box
+            if ok:
+                # Tracking success
+                p1 = (int(roi[0]), int(roi[1]))
+                p2 = (int(roi[0] + roi[2]), int(roi[1] + roi[3]))
+                cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+            else:
+                # Tracking failure
+                cv2.putText(frame, "Tracking failure detected", (100, 80),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+            qrframe = frame
+        elif track_fail_count < track_timeout:
+            # https://stackoverflow.com/questions/42004652/how-can-i-find-contours-inside-roi-using-opencv-and-python
+            black = np.zeros((frame.shape[0], frame.shape[1], 3), np.uint8)
+            cv2.circle(black, lastCentre, 200, (255,255,255), -1)
+            gray = cv2.cvtColor(black, cv2.COLOR_BGR2GRAY)
+            ret, b_mask = cv2.threshold(gray, 127, 255, 0)
+            qrframe = cv2.bitwise_and(frame, frame, mask=b_mask)
         else:
-            # Tracking failure
-            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+            qrframe = frame
 
         # Detect the qrcode
-        found,bbox = qrDecoder.detect(frame)
+        found, bbox = qrDecoder.detect(frame)
         if found:
+            bbox = bbox[0]  # bbox is always a unit length list, so just grab the first element
             shape_data = getQRShape(bbox)
             isValid = shape_data[0] < 300**2 and shape_data[0] > 60**2 and shape_data[1] > 0.98
             # text_data = getQRData(frame, bbox, qrDecoder)
             # isValid = text_data == "bit.ly/3tbqjqL"
 
             # Draw blue border if valid, green otherwise.
-            drawMarkers(frame, bbox, (255,0,0) if isValid else (0,255,0))
+            drawMarkers(frame, bbox, (255, 0, 0) if isValid else (0, 255, 0))
+            if not TRACKER:
+                if isValid:
+                    lastCentre = np.mean(bbox, axis=0).astype(int)
+                    track_fail_count = 0
+                else:
+                    track_fail_count += 1
         else:
-            cv2.putText(frame, "QR code not detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+            cv2.putText(frame, "QR code not detected", (100, 80),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
         # Calculate Frames per second (FPS)
         fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
 
         # Display FPS on frame
-        cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2)
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
         # Display result
-        cv2.imshow("Tracking", frame)
+        cv2.imshow("Tracking", b_mask)
 
         # Exit if ESC pressed
         # cv2.waitKey(0)
-        if cv2.waitKey(1) & 0xFF == ord('q'): # if press SPACE bar
+        if cv2.waitKey(1) & 0xFF == ord('q'):  # if press SPACE bar
             break
 
     video.release()
