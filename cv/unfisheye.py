341e614706b256812b6459845a4d1d0c1c3079c2
Nik Lebedenko <nl419@cam.ac.uk>
2022-02-26 23:05:11 +0000

    added window resizing

--- cv/unfisheye.py
+++ cv/unfisheye.py
@@ -11,28 +11,33 @@ import imutils
 # pic_name = "cv/1.jpg"
 
 # Windows checkerboard
-# DIM=(903, 720)
-# K=np.array([[497.25261908023276, 0.0, 451.0843444150783], [0.0, 495.86040011896534, 385.5926770729965], [0.0, 0.0, 1.0]])
-# D=np.array([[-0.09268730609892956], [0.2504965645950943], [-0.3533145752831843], [0.16524591721022114]])
-# pic_name = "cv/1.jpg"
+DIM=(903, 720)
+K=np.array([[497.25261908023276, 0.0, 451.0843444150783], [0.0, 495.86040011896534, 385.5926770729965], [0.0, 0.0, 1.0]])
+D=np.array([[-0.09268730609892956], [0.2504965645950943], [-0.3533145752831843], [0.16524591721022114]])
+pic_name = "cv/1.jpg"
 
 # Windows checkerboard2
-DIM=(1016, 760)
-K=np.array([[617.046363722009, 0.0, 508.1742575190148], [0.0, 614.3819211597321, 382.7568542470966], [0.0, 0.0, 1.0]])
-D=np.array([[-0.23284420692502958], [0.7725973070172576], [-1.2587721414504571], [0.7234483916379288]])
-pic_name = "checkerboard2/3.jpg"
+# DIM=(1016, 760)
+# K=np.array([[617.046363722009, 0.0, 508.1742575190148], [0.0, 614.3819211597321, 382.7568542470966], [0.0, 0.0, 1.0]])
+# D=np.array([[-0.23284420692502958], [0.7725973070172576], [-1.2587721414504571], [0.7234483916379288]])
+# pic_name = "checkerboard2/3.jpg"
 
 debug = __name__ == "__main__"
 
 def undistort(img):
     img = cv2.resize(img, DIM)
     if debug:
+        cv2.namedWindow("input", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("input", DIM[0], DIM[1])
         cv2.imshow("input", img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
     undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
     if debug:
+        cv2.namedWindow("undistorted", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("undistorted", DIM[0], DIM[1])
+        cv2.rectangle(undistorted_img, (1,1), (DIM[0] - 2, DIM[1] - 2), (255,0,0), 1)
         cv2.imshow("undistorted", undistorted_img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
