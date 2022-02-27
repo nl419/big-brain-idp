c0ace5b576086fe7321632bd754ef39e191fe611
niklebedenko <nananananabatman>
2022-02-27 14:12:43 +0000

    corner detection working nicely

--- cv/find_corners.py
+++ cv/find_corners.py
@@ -1,49 +1,45 @@
-import cv2, queue, threading, time
+"""Show all the corners found with cv2.goodFeaturesToTrack.
+Sensitive to noise, so """
 
-CHESSBOARD = (3,3)
-
-# bufferless VideoCapture
-class VideoCapture:
-
-  def __init__(self, name):
-    self.cap = cv2.VideoCapture(name)
-    self.q = queue.Queue()
-    t = threading.Thread(target=self._reader)
-    t.daemon = True
-    t.start()
-
-  # read frames as soon as they are available, keeping only most recent one
-  def _reader(self):
-    while True:
-      ret, frame = self.cap.read()
-      if not ret:
-        break
-      if not self.q.empty():
-        try:
-          self.q.get_nowait()   # discard previous (unprocessed) frame
-        except queue.Empty:
-          pass
-      self.q.put(frame)
-
-  def read(self):
-    return self.q.get()
+import cv2
+from laggy_video import VideoCapture
+import numpy as np
+from unfisheye import undistort
 
 cap = VideoCapture('http://localhost:8081/stream/video.mjpeg')
+USE_HARRIS = False # whether to use Harris corner detection
+
+CORNER_CLOSENESS = 5 # Minimum distance between corners
+CORNER_QUALITY = 0.1
+CORNER_NUMBER = 100
+BLUR_RAD = 5 # Blur radius for dealing with noise (only used if USE_HARRIS == False)
+
 while True:
-    #time.sleep(.5)   # simulate time between events
     frame = cap.read()
+    frame = undistort(frame, 0.4)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Basic corner detection
-    # gray = cv2.cvtColor(frame, COLOR_BGR2GRAY)
-    # gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-    #                 cv2.THRESH_BINARY,401,2)
-    # corners = cv2.cornerHarris(gray, 2,3,0.04)
-    # corners = cv2.dilate(corners, None)
-    # frame[corners>0.01*corners.max()] = [0,0,255]
-
-    ret, corners = cv2.findChessboardCorners(frame, CHESSBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
-    frame = cv2.drawChessboardCorners(frame, CHESSBOARD, corners, ret)
-    print(corners)
+    if USE_HARRIS:
+      # Create binary image
+      gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                      cv2.THRESH_BINARY,401,2)
+      # Get corner image (brighter == more corner-y)
+      corners = cv2.cornerHarris(gray, 5,3,0.04)
+      corners = cv2.dilate(corners, None)
+      # Set frame to red at corners
+      frame[corners>0.01*corners.max()] = [0,0,255]
+    else:
+      # Blur image to filter out noise
+      blurred = cv2.blur(gray, (BLUR_RAD,BLUR_RAD))
+      # Get corner locations
+      corners = cv2.goodFeaturesToTrack(blurred, CORNER_NUMBER, CORNER_QUALITY, CORNER_CLOSENESS)
+      # Draw corners
+      corners = np.int0(corners)
+      for corner in corners:
+        # print(corner)
+        cv2.circle(frame, corner[0], 5, (0,0,255), 2)
+      # print("===")
+    
     cv2.imshow("frame", frame)
     if chr(cv2.waitKey(1)&255) == 'q':
         break
