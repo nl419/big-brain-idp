ff3f6f324b0bf7a667f48beee6fc7ae0fa4fa6cb
Nik Lebedenko <nl419@cam.ac.uk>
2022-03-07 21:50:20 +0000

    remove colour finding

--- cv/find_block.py
+++ cv/find_block.py
@@ -8,7 +8,7 @@ _DEBUG = __name__ == "__main__"
 
 def find_block(image: np.ndarray, shift: np.ndarray, invmat: np.ndarray, angle_offset: float = 0):
     """Find a single block in the pickup area. 
-    Will return None, None, False if no blocks or multiple blocks detected.
+    Will return None, None if no blocks or multiple blocks detected.
 
     Parameters
     ----------
@@ -27,8 +27,6 @@ def find_block(image: np.ndarray, shift: np.ndarray, invmat: np.ndarray, angle_o
         Centre of the block, if found, else None
     forward : np.ndarray or None
         Forward direction of the block, if found, else None
-    is_blue : bool
-        Whether the block is blue
     """
     
     # Mask the pickup area
@@ -59,7 +57,7 @@ def find_block(image: np.ndarray, shift: np.ndarray, invmat: np.ndarray, angle_o
 
     # If no block found, or more than one block found
     if block is None or len(centres) > 1:
-        return None, None, False
+        return None, None
 
     # Get orientation
     mid,_,angle = cv2.minAreaRect(block)
@@ -67,42 +65,8 @@ def find_block(image: np.ndarray, shift: np.ndarray, invmat: np.ndarray, angle_o
     forward = np.array((np.sin(np.radians(angle)), -np.cos(np.radians(angle))))
     _DEBUG and print(f"forward {forward}")
     # so forward now points in the direction that the robot should come from
-
-    # Mask the blue areas
-    hMin = 40; sMin = 100; vMin = 80; hMax = 140; sMax = 255; vMax = 255
-    hsv = cv2.cvtColor(cropped, cv2.COLOR_BGR2HSV)
-    lower = np.array([hMin, sMin, vMin])
-    upper = np.array([hMax, sMax, vMax])
-    mask_blue = cv2.inRange(hsv, lower, upper)
-
-    if _DEBUG:
-        cv2.imshow("mask_blue", mask_blue)
-        cv2.waitKey(0)
-        cv2.destroyWindow("mask_blue")
-
-    mask_near_block = cv2.circle(np.zeros_like(mask_blue, np.uint8), np.int0(mid), 15, 255, -1)
-    mask_blue = cv2.bitwise_and(mask_blue, mask_near_block)
-
-    if _DEBUG:
-        cv2.imshow("mask_near_block", mask_near_block)
-        cv2.imshow("mask_blue", mask_blue)
-        cv2.waitKey(0)
-        cv2.destroyWindow("mask_blue")
-        cv2.destroyWindow("mask_near_block")
-
-    # Get blue contours - if there is a large contour, then the block is blue
-    cnts, _ = cv2.findContours(mask_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    centres = []
-    areas = []
-    blue = False
-    for c in cnts:
-        M = cv2.moments(c)
-        area = M['m00']
-        _DEBUG and print(area)
-        if area > 10:
-            blue = True
     
-    return mid, forward, blue
+    return mid, forward
     
 def _test_rectangle_orientation():
     # A very basic example demonstrating orientation detection
@@ -171,7 +135,7 @@ def _test_find_block():
     cv2.imshow("before", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    mid, forward, blue = find_block(image, shift, invmat, 0)
+    mid, forward = find_block(image, shift, invmat, 0)
 
     if mid is None:
         cv2.imshow("no block!", image)
@@ -180,13 +144,12 @@ def _test_find_block():
 
     front = mid + forward * 80
 
-    cv2.drawMarker(image, np.int0(mid), (255,0,0) if blue else (0,0,255), cv2.MARKER_CROSS,
+    cv2.drawMarker(image, np.int0(mid), (0,0,255), cv2.MARKER_CROSS,
                    40, 2)
     cv2.line(image, np.int0(mid), np.int0(front), (0,255,0), 4)
 
     print(mid)
     print(forward)
-    print(blue)
 
     cv2.imshow("block found", image)
     cv2.waitKey(0)
